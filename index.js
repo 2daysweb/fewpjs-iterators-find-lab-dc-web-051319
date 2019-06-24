97d8cc67364a6b483488a83a7f7810f64a9dc989
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-24 01:24:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(recordHash)
+{
+  
+  let champsHash = recordHash.find(e => e.result === "W");
+  
+  if (champsHash.typeOf() === undefined) {
+  
+    return undefined 
+    
+  }
+    else {
+  return champsHash.year  
+    }  
+}
